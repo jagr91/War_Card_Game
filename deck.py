bd17bafc0f78f04c3b88f0b23f962c2a64cbdb40
Grzegorz Jastrzebski <gjastrzebski91@gmail.com>
2022-04-09 19:03:06 +0200

    move deck creation and shufle to a new file

---
+++ deck.py
@@ -0,0 +1,10 @@
+def deck():
+    import random
+    """combine 52 card decks by figure+colour e.g. "A♦" and shuffle it"""
+
+    figures = ["2", "3", "4", "5", "6", "7",
+               "8", "9", "10", "J", "Q", "K", "A"]
+    colours = ["♠", "♦", "♥", "♣"]
+    deck = [(str(f)+c) for f in figures for c in colours]
+    random.shuffle(deck)
+    return deck
